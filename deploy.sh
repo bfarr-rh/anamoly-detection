707ac03da4be9a447f4e18b8baf19445c553cf42
bfarr-rh <bfarr@redhat.com>
2022-09-15 09:37:06 +1000

    added commands

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,20 @@ oc apply -f ./deploy/Seldon-Deployment.yaml
 
 oc apply -f ./deploy/minio-full.yaml
 
-oc new-app https://github.com/bfarr-rh/anamoly-detection.git  --context-dir=frontend  --name=dashboard -e MINIO_URL=http://minio-ml-workshop:9000
+oc new-app https://github.com/bfarr-rh/anamoly-detection.git  --context-dir=dashboard  --name=dashboard -e MINIO_URL=http://minio-ml-workshop:9000
 oc create route edge --service=dashboard
 
 oc new-app https://github.com/bfarr-rh/anamoly-detection.git  --context-dir=event-producer-js --name=frontend -e CAPTURE_INTERVAL=$CAPTURE_INTERVAL -e KAFKA_BROKER_URL=$KAFKA_BROKER_URL -e SASL_USERNAME=$SASL_USERNAME -e SASL_PASSWORD=$SASL_PASSWORD
 
 oc create route edge --service=frontend
+
+oc apply -f ./deploy/postgres.yaml
+oc apply -f ./deploy/postgreslocalyaml
+
+oc new-app sosedoff/pgweb
+oc create route edge --service=pgweb
+
+#oc cp ./deploy/preload.sql `oc get pods -l app=postgres -o json | jq -r '.items[].metadata.name'`:/opt 
+
+#oc exec `oc get pods -l app=postgres -o json | jq -r '.items[].metadata.name'` --  psql -d db -U user < /opt/preload.sql
 
